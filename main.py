a43511d3fa85ce0be7f7349ef35ae6631e67f93f
Ajinkya8881 <121487681+Ajinkya8881@users.noreply.github.com>
2022-12-27 23:29:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import streamlit as st
 from PIL import Image
+st.subheader("Color to Grayscale Converter")
+uploaded_image = st.file_uploader("Upload Image")
 with st.expander("Start Camera"):
 
     camera_image = st.camera_input("Camera")
@@ -10,3 +12,10 @@ if camera_image:
     gray_img = img.convert("L")
 
     st.image(gray_img)
+
+if uploaded_image:
+    img = Image.open(uploaded_image)
+
+    gray_uploaded_img = img.convert("L")
+
+    st.image(gray_uploaded_img)
